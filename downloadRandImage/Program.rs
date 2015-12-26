ce82b8faa4c446e174790db9e6157399effb469e
sentenzo <sentenzo@users.noreply.github.com>
2015-12-26 04:03:35 +0300

    ugly Rust code

--- downloadRandImage/Program.rs
+++ downloadRandImage/Program.rs
@@ -4,10 +4,10 @@ extern crate regex;
 use curl::http;
 use regex::Regex;
 use std::io::prelude::*;
+use std::io::Error;
 use std::fs::File;
 
 static URL: &'static str = "http://alpha.wallhaven.cc/random";
-
 static REX: &'static str = r#"<img [^>]*? src=".*?th-(\d+\..{3})"[^>]*?>"#;
 
 fn get_body(url : &str) -> String {
@@ -24,21 +24,28 @@ fn get_body(url : &str) -> String {
     return ret; 
 }
 
-fn get_img_name(html : &str) -> String {
+fn get_img_name(html : &str) -> (String, String) {
     let re = Regex::new(REX).unwrap();
     let cap = re.captures(html).unwrap();
-    let mut ret_a = "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-".to_string();
+    let ret_a = "http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-".to_string();
     let ret_b = cap.at(1).unwrap_or("");
     let ret = ret_a+ret_b; // :0
-    return ret;
+    return (ret, ret_b.to_string());
 }
-
-//fn download_img(url : str) {
-//    let mut f = try!(File::create("foo.jpg"));
-//}
+ 
+fn download_img(url : &str, name : &str)  -> Result<(), Error>  {
+    let resp = http::handle()
+        .get(url)
+        .exec()
+        .unwrap();
+    let mut f = try!(File::create(name));
+    try!(f.write_all(resp.get_body()));
+    Ok(())
+}
 
 fn main() {
     let html = get_body(URL);
-    let img = get_img_name(&html);
-    println!("{}", img); 
+    let (img_url,img_name) = get_img_name(&html);
+    download_img(&img_url, &img_name);
+    //println!("{}", img); 
 }
